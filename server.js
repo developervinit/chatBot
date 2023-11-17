b8c77479a7a41c8eaa6740b1958c3e04eff3e0b2
developervinit <vinit91.dev@gmail.com>
2023-11-17 20:02:49 +0530

    stablished a connection with mongoDB

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("./app");
 const http = require("http");
 const path = require("node:path");
 const { Server } = require("socket.io");
+const mongoose = require('mongoose');
 require("dotenv").config();
 
 let server = http.createServer(app);
@@ -9,6 +10,28 @@ let io = new Server(server);
 
 // app.use(express.static(path.dirname("./public/index.html")));
 let join = path.join;
+
+//start-code to connect mongodb
+async function connectingMongoose(dbString){
+    try{
+        await mongoose.connect(dbString);
+    }catch(error){
+        console.error('Error connecting to MongoDB:', error);
+    }
+    
+}
+
+const db = mongoose.connection;
+db.on("connected", () => {
+    console.log("Connected to MongoDB Successfully");
+});
+db.on("error", (err) => {
+    console.error(`Error connecting to mongoDB ${err}`);
+})
+
+let dbString = process.env.CONNECTIONSTRING.replace("password", process.env.PASSWORD);
+connectingMongoose(dbString);
+//end-code to connect mongodb
 
 
 //attaching static file on home-route
