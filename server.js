e395c4e73aac3626a527fb2aae8c2a387d3934c1
developervinit <vinit91.dev@gmail.com>
2023-11-17 19:41:11 +0530

    stablished communication between client and server using socket.io

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const app = require("./app");
 const http = require("http");
 const path = require("node:path");
+const { Server } = require("socket.io");
 require("dotenv").config();
 
 let server = http.createServer(app);
+let io = new Server(server);
 
 // app.use(express.static(path.dirname("./public/index.html")));
 let join = path.join;
@@ -12,6 +14,13 @@ let join = path.join;
 //attaching static file on home-route
 app.get("/", (req, res) => {
     res.sendFile(join(__dirname, "public/index.html"));
+});
+
+//sending response to client using socket.io
+io.on('connection', (socket) => {
+    socket.on('chat message', async (msg) => {
+        io.emit('chat message', msg);
+    });
 });
 
 
