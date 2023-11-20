553321615c8143006d5bea23ac6efec30f000ee4
developervinit <vinit91.dev@gmail.com>
2023-11-20 22:27:43 +0530

    handled undefined routes

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const historyDataRoute = require('./routes/historyDataRoute');
 let app = express();
 app.use(express.json()); //to handle post request.
 
-app.use(express.static("public")); //to attach the static file.
+//to attach the static file. index.html file in public folder runs on "/" directory.
+app.use(express.static("public")); 
 
 //mounting the routes.
 app.use('/openai', openAiRoute);
 app.use('/history', historyDataRoute);
+
+app.all("*", (req, res) => {
+    res.send("Opps this route is not defined");
+})
 
 
 module.exports = app;
